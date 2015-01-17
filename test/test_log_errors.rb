4f6ad8c64c89f8e142217924f7ad64d4d4850c07
Tim Uruski <github@tmu.pw>
2015-01-16 21:50:27 -0700

    Test logging of LogErrors middleware

--- test/test_log_errors.rb
+++ test/test_log_errors.rb
@@ -1,24 +1,36 @@
 require_relative 'test_helper'
 require_relative '../lib/log_errors'
+require 'stringio'
+require 'logger'
 
 class TestLogErrors < Minitest::Test
   # When an exception is raised, it rescues it and responds with 500.
-  # When an exception is raised, it outputs an appropriate log message.
   def test_rescues_errors
     app = AppHarness.new(LogErrors) do
       raise 'Kaboom'
     end
 
     begin
-      status, headers, body = app.call
-
-      assert_equal status, 500
+      app.call
     rescue RuntimeError => error
       # Minitest has no refute_raises, because reasons!
       # So we capture any raised exception and then assert it should not have
       # been raised.
       flunk 'did not rescue error'
     end
+  end
+
+  # When an exception is raised, it outputs an appropriate log message.
+  def test_logs_errors
+    app = AppHarness.new(LogErrors) do
+      raise 'Kaboom'
+    end
+
+    output = StringIO.new('')
+    status, _, _ = app.call('/', 'rack.logger' => Logger.new(output))
+
+    assert_equal status, 500
+    assert_match /ERROR -- : boom/, output.string
   end
 
   # When no exception is raised, it does nothing.
