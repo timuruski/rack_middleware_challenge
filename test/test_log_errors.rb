7d186cdd4563ef5058ebd5449871d6fab0556326
Tim Uruski <github@tmu.pw>
2015-01-18 19:21:36 -0700

    Improve tests for LogErrors

--- test/test_log_errors.rb
+++ test/test_log_errors.rb
@@ -4,32 +4,61 @@ require 'stringio'
 require 'logger'
 
 class TestLogErrors < Minitest::Test
+  # Runs a block, capturing IO and flunking on raise.
+  #   output = StringIO.new
+  #   status, headers, body = without_error(output) { app.call }
+  #   assert_equal 200, status
+  #   assert output.empty?
+  def without_error(captured_err = StringIO.new)
+    original_err = $stderr
+    $stderr = captured_err
+
+    yield
+  rescue
+    flunk 'Exception not rescued'
+  ensure
+    $stderr = original_err
+  end
+
   # When an exception is raised, it rescues it and responds with 500.
   def test_rescues_errors
     app = AppHarness.new(LogErrors) do
       raise 'Kaboom'
     end
 
-    begin
+    without_error do
       app.call
-    rescue RuntimeError => error
-      # Minitest has no refute_raises, because reasons!
-      # So we capture any raised exception and then assert it should not have
-      # been raised.
-      flunk 'did not rescue error'
     end
   end
 
-  # When an exception is raised, it outputs an appropriate log message.
-  def test_logs_errors
+  # When an exception is raised, it outputs an appropriate log message to
+  # rack.logger
+  def test_logs_errors_to_rack_logger
     app = AppHarness.new(LogErrors) do
       raise 'Kaboom'
     end
 
-    output = StringIO.new('')
-    status, _, _ = app.call('/', 'rack.logger' => Logger.new(output))
+    logger_output = StringIO.new
+    without_error do
+      app.call('/', 'rack.logger' => Logger.new(logger_output))
+    end
 
-    assert_match /ERROR -- : Kaboom/, output.string
+    assert_match /ERROR -- : Kaboom/, logger_output.string
+  end
+
+  # When an exception is raised, and rack.logger is missing, it outputs to
+  # STDERR.
+  def test_logs_errors_to_stderr
+    app = AppHarness.new(LogErrors) do
+      raise 'Kaboom'
+    end
+
+    captured_errs = StringIO.new
+    without_error(captured_errs) do
+      app.call
+    end
+
+    assert_match /ERROR -- : Kaboom/, captured_errs.string
   end
 
   # When an exception is raised, it returns an appropriate response.
@@ -38,8 +67,9 @@ class TestLogErrors < Minitest::Test
       raise 'Kaboom'
     end
 
-    output = StringIO.new('')
-    status, _, body = app.call
+    status, _, body = without_error do
+      app.call
+    end
 
     assert_equal 500, status
     assert_equal ['Internal Server Error'], body
@@ -51,7 +81,9 @@ class TestLogErrors < Minitest::Test
       [200, {}, ['Hello, world']]
     end
 
-    status, _, body = app.call
+    status, _, body = without_error do
+      app.call
+    end
 
     assert_equal 200, status
     assert_equal ['Hello, world'], body
