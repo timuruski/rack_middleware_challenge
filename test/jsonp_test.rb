30c29532766fa6e425e371b62ec0e3570bd49a5e
Tim Uruski <github@tmu.pw>
2015-01-24 15:36:56 -0700

    Test JsonP middleware for content-length changes

--- test/jsonp_test.rb
+++ test/jsonp_test.rb
@@ -5,7 +5,7 @@ class TestJsonP < Minitest::Test
   # When callback is provided and response content-type is JSON
   def test_wraps_callback
     app = AppHarness.new(JsonP) do |env|
-      [200, {'Content-type' => 'application/json'}, ['{"user": "alice"}']]
+      [200, {'Content-Type' => 'application/json'}, ['{"user": "alice"}']]
     end
 
     _, _, body = app.call('/users/123', params: {'callback' => 'parseUser'})
@@ -14,10 +14,26 @@ class TestJsonP < Minitest::Test
     assert_equal expected_body, body
   end
 
+  # When callback is provided, the content-length is adjusted.
+  def test_content_length
+    app = AppHarness.new(JsonP) do |env|
+      body = '{"user": "alice"}'
+      headers = {'Content-Type' => 'application/json',
+                 'Content-Length' => body.length}
+
+      [200, headers, [body]]
+    end
+
+    _, headers, _ = app.call('/users/123', params: {'callback' => 'parseUser'})
+
+    expected_length = 'parseUser({"user": "alice"});'.length
+    assert_equal expected_length, headers['Content-Length']
+  end
+
   # When callback is not provided, it does nothing
   def test_missing_callback
     app = AppHarness.new(JsonP) do |env|
-      [200, {'Content-type' => 'application/json'}, ['{"user": "alice"}']]
+      [200, {'Content-Type' => 'application/json'}, ['{"user": "alice"}']]
     end
 
     _, _, body = app.call('/users/123')
@@ -27,9 +43,9 @@ class TestJsonP < Minitest::Test
   end
 
   # When response content-type is not JSON, it does nothing
-  def test_harness
+  def test_non_json_response
     app = AppHarness.new(JsonP) do |env|
-      [200, {'Content-type' => 'test/html'}, ['<p>User: alice</p>']]
+      [200, {'Content-Type' => 'test/html'}, ['<p>User: alice</p>']]
     end
 
     _, _, body = app.call('/users/123')
